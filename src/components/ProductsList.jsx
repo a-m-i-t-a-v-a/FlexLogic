184bc855b01a0172e73e0cd62008fc942fc9436b
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-11-06 21:19:08 +0530

    Adding the app styling and skeletal layout

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,8 +1,24 @@
 const ProductsList = () => {
+  const DUMMY_ARRAY=Array.from({length:30})
   return (
-    <div>
-      Products list
-    </div>
+    <main className="product-list">
+       <div className="product-header">
+         <p>Showing 10 out of 18 products</p>
+         <select>
+            <option value="relevance">Most Relevant</option>
+            <option value="price-asc">Price : Low to High</option>
+            <option value="price-desc">Price : High to Low</option>
+         </select>
+       </div>
+       <div className="product-grid">
+          {DUMMY_ARRAY.map((_,index)=>(
+            <div key={index} className="product-card">
+              <h3>Product : {index+1}</h3>
+              <p>Price $0</p>
+            </div>
+          ))}
+       </div>
+    </main>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,21 @@
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
-    </div>
+    <aside className="sidebar">
+      <h2>CATEGORIES</h2>
+      <input type="checkbox"/>
+      <label>Electronics & Gadgets</label>
+      <h2>PRICE</h2>
+      <input type="range" min="0" max="1500"/>
+      <p>Price range $0 - $1500</p>
+      <h2>BRANDS</h2>
+      <input type="checkbox"/>
+      <label>Bose</label>
+      <h2>AVAILABLITY</h2>
+      <select>
+        <option>In Stock</option>
+        <option>Out of stock</option>
+      </select>
+    </aside>
   )
 }
 
